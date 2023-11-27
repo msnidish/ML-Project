50a158853b8a1627a901180ec25c60cf0bb695cc
msnidish <msnidish@gmail.com>
2023-11-27 06:48:01 -0800

    third Commit

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## Steps to Start with
 Creating a Virtual Environment
-1) TO Create a Virtual Environment where all the required packages for the project are installed we use Anaconda tool
+To Create a Virtual Environment where all the required packages for the project are installed we use Anaconda tool
 
 Command: conda create -p venv python==3.8 -y
+
+Syncing with GitHib 
+
+Creating a setup.py which acts as a package for buiding the maching learning algorithm
 
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+CONST='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if CONST in requirements:
+            requirements.remove(CONST)
+    return requirements
+
+
+setup(
+name='MLProject',
+version='0.0.1',
+author='Nidish',
+author_email="msnidish@gmail.com",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
